18814ca48c47d9177d008dfc0dff41234ac29fc5
Leo Tong <leo.tong@student.unsw.edu.au>
2025-02-19 16:18:38 +1100

    remove punctuation option

--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -4,4 +4,5 @@ export default {
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
+  testPathIgnorePatterns: ["tests/HelperMethod.test.ts"]
 };
--- typescript/tests/ExtractChinese.test.ts
+++ typescript/tests/ExtractChinese.test.ts
@@ -1,11 +1,199 @@
-import { extractChinese } from '../src/ExtractChinese'
+import { extractChinese } from "../src/ExtractChinese";
 
-describe("Tests for extractChinese", () => {
+type TestCase = [name: string, input: string, expected: Record<string, string>];
 
-})
+const cases: TestCase[] = [
+  [
+    "Empty String",
+    "",
+    {
+      default: "",
+      normalizationDisabled: "",
+      punctuationEnabled: "",
+    },
+  ],
+  [
+    "Punctuation Only",
+    "。？！，、；：“”‘’『』「」（）[]〔〕【】—‖·-～《》〈〉_/",
+    {
+      default: "",
+      normalizationDisabled: "",
+      punctuationEnabled:
+        "。？！，、；：“”‘’『』「」（）[]〔〕【】—‖·-～《》〈〉_/",
+    },
+  ],
+  [
+    "Other Symbols/Letters",
+    "!@#$%^&*()1234567890-=_+[]{}|;':\",./<>?",
+    {
+      default: "",
+      normalizationDisabled: "",
+      punctuationEnabled: "-_[]/",
+    },
+  ],
+  [
+    "All Simplified Characters",
+    "全民制作人们大家好",
+    {
+      default: "全民制作人们大家好",
+      normalizationDisabled: "全民制作人们大家好",
+      punctuationEnabled: "全民制作人们大家好",
+    },
+  ],
+  [
+    "All Traditional Characters",
+    "練習時常兩年半的個人練習生",
+    {
+      default: "練習時常兩年半的個人練習生",
+      normalizationDisabled: "練習時常兩年半的個人練習生",
+      punctuationEnabled: "練習時常兩年半的個人練習生",
+    },
+  ],
+  [
+    "Mixed Simplified Traditional",
+    "全民制作人们大家好我是練習時常兩年半的個人練習生",
+    {
+      default: "全民制作人们大家好我是練習時常兩年半的個人練習生",
+      normalizationDisabled: "全民制作人们大家好我是練習時常兩年半的個人練習生",
+      punctuationEnabled: "全民制作人们大家好我是練習時常兩年半的個人練習生",
+    },
+  ],
+  [
+    "All English",
+    "The Quick Brown Fox Jumped Over the Lazy Dog",
+    {
+      default: "",
+      normalizationDisabled: "",
+      punctuationEnabled: "",
+    },
+  ],
+  [
+    "Mixed English/Chinese",
+    "Super idol的笑容都没你的甜八月正午的阳光都没你耀眼热爱105度的你滴滴清纯的蒸馏水",
+    {
+      default:
+        "的笑容都没你的甜八月正午的阳光都没你耀眼热爱度的你滴滴清纯的蒸馏水",
+      normalizationDisabled:
+        "的笑容都没你的甜八月正午的阳光都没你耀眼热爱度的你滴滴清纯的蒸馏水",
+      punctuationEnabled:
+        "的笑容都没你的甜八月正午的阳光都没你耀眼热爱度的你滴滴清纯的蒸馏水",
+    },
+  ],
+  [
+    "No Duplicates Removed",
+    "重复重复重复重复重复重复甲乙丙丁戊己庚辛甲乙丙丁戊己庚辛練習生練習生。？！，、；：“”‘’『』「」（）[]〔〕【】—‖·-～《》〈〉_/'!@#$%^&*()1234567890=+{}|;:\",.<>?",
+    {
+      default:
+        "重复重复重复重复重复重复甲乙丙丁戊己庚辛甲乙丙丁戊己庚辛練習生練習生",
+      normalizationDisabled:
+        "重复重复重复重复重复重复甲乙丙丁戊己庚辛甲乙丙丁戊己庚辛練習生練習生",
+      punctuationEnabled:
+        "重复重复重复重复重复重复甲乙丙丁戊己庚辛甲乙丙丁戊己庚辛練習生練習生。？！，、；：“”‘’『』「」（）[]〔〕【】—‖·-～《》〈〉_/",
+    },
+  ],
+  [
+    "Whitespace only",
+    " \t \n ",
+    { default: "", normalizationDisabled: "", punctuationEnabled: "" },
+  ],
+  [
+    "Mixed Whitespace & Chinese",
+    " 你好 世界 ",
+    {
+      default: "你好世界",
+      normalizationDisabled: "你好世界",
+      punctuationEnabled: "你好世界",
+    },
+  ],
+  [
+    "Different Whitespace Encodings",
+    "你\u3000好\u00A0世\u200B界",
+    {
+      default: "你好世界",
+      normalizationDisabled: "你好世界",
+      punctuationEnabled: "你好世界",
+    },
+  ],
+  [
+    "Emojis",
+    "🎵跳rap籃球music雞你太💄",
+    {
+      default: "跳籃球雞你太",
+      normalizationDisabled: "跳籃球雞你太",
+      punctuationEnabled: "跳籃球雞你太",
+    },
+  ],
+  [
+    "Other CJK characters",
+    "漢字（中文）、漢字（日本語）、한글（한국어）",
+    {
+      default: "漢字中文漢字日本語",
+      normalizationDisabled: "漢字中文漢字日本語",
+      punctuationEnabled: "漢字（中文）、漢字（日本語）、（）",
+    },
+  ],
+  [
+    "CJK Unified + Expansion Characters A-D",
+    "鶨鶻鶑秉秌禪㐖㐤䏛䤙䳓䴓㕣䶹\u{23C6B}\u{25D6F}\u{2760E}\u{28547}\u{2937F}\u{2a709}\u{2a74a}\u{2b3ca}\u{2b5e6}\u{2b6c8}\u{2B7B8}\u{2B7B0}",
+    {
+      default:
+        "鶨鶻鶑秉秌禪㐖㐤䏛䤙䳓䴓㕣䶹\u{23C6B}\u{25D6F}\u{2760E}\u{28547}\u{2937F}\u{2a709}\u{2a74a}\u{2b3ca}\u{2b5e6}\u{2b6c8}\u{2B7B8}\u{2B7B0}",
+      normalizationDisabled:
+        "鶨鶻鶑秉秌禪㐖㐤䏛䤙䳓䴓㕣䶹\u{23C6B}\u{25D6F}\u{2760E}\u{28547}\u{2937F}\u{2a709}\u{2a74a}\u{2b3ca}\u{2b5e6}\u{2b6c8}\u{2B7B8}\u{2B7B0}",
+      punctuationEnabled:
+        "鶨鶻鶑秉秌禪㐖㐤䏛䤙䳓䴓㕣䶹\u{23C6B}\u{25D6F}\u{2760E}\u{28547}\u{2937F}\u{2a709}\u{2a74a}\u{2b3ca}\u{2b5e6}\u{2b6c8}\u{2B7B8}\u{2B7B0}",
+    },
+  ],
+  [
+    "Expansion Characters E-I",
+    "\u{2B820}\u{2B865}\u{2BF9E}\u{2CEB0}\u{2E907}\u{2EBE0}\u{3002E}\u{30000}\u{3134A}\u{31350}\u{3184C}\u{323AF}\u{2EBF0}\u{2EE5D}\u{2EC7C}",
+    {
+      default:
+        "\u{2B820}\u{2B865}\u{2BF9E}\u{2CEB0}\u{2E907}\u{2EBE0}\u{3002E}\u{30000}\u{3134A}\u{31350}\u{3184C}\u{323AF}\u{2EBF0}\u{2EE5D}\u{2EC7C}",
+      normalizationDisabled:
+        "\u{2B820}\u{2B865}\u{2BF9E}\u{2CEB0}\u{2E907}\u{2EBE0}\u{3002E}\u{30000}\u{3134A}\u{31350}\u{3184C}\u{323AF}\u{2EBF0}\u{2EE5D}\u{2EC7C}",
+      punctuationEnabled:
+        "\u{2B820}\u{2B865}\u{2BF9E}\u{2CEB0}\u{2E907}\u{2EBE0}\u{3002E}\u{30000}\u{3134A}\u{31350}\u{3184C}\u{323AF}\u{2EBF0}\u{2EE5D}\u{2EC7C}",
+    },
+  ],
+  [
+    "CJK Compatability Normalization",
+    "\uf900\uF91C\uF9B5\u{2F800}\u{2F9E0}",
+    {
+      default: "\u8c48\u5375\u4F8B\u4E3D\u{285D2}",
+      normalizationDisabled: "\uf900\uF91C\uF9B5\u{2F800}\u{2F9E0}",
+      punctuationEnabled: "\uf900\uF91C\uF9B5\u{2F800}\u{2F9E0}",
+    },
+  ],
+  [
+    "Rare Characters",
+    "𠀀𠜎𡃁",
+    {
+      default: "𠀀𠜎𡃁",
+      normalizationDisabled: "𠀀𠜎𡃁",
+      punctuationEnabled: "𠀀𠜎𡃁",
+    },
+  ],
+];
 
-test("basic test", () => {
-  expect(extractChinese("逼逼赖赖", { removeDuplicates: true })).toBe("逼赖")
-})
+describe.each(cases)(
+  "Test extractChinese with default params",
+  (name, input, expected) => {
+    test(name, () => {
+      // Default cases
+      expect(extractChinese(input)).toBe(expected.default);
 
+      // Disable Unicode Normalization
+      expect(extractChinese(input, { normalizeUnicode: false })).toBe(
+        expected.normalizationDisabled
+      );
 
+      // Enable Punctuation
+      expect(
+        extractChinese(input, { removePunctuation: false })
+      ).toBe(expected.punctuationEnabled);
+
+      // Remove Duplicates
+    });
+  }
+);
